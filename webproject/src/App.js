947229a989cc0ffb95f34f5f5fd1c19d24cc35a7
Prabhat Thakur <thakurprabhat2012@gmail.com>
2020-07-27 17:10:42 +0530

    started working on blog

---
+++ webproject/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Blog from './components/screens/blog';
+
+function App() {
+  return (
+    <div >
+      <Blog/>
+    </div>
+  );
+}
+
+export default App;
